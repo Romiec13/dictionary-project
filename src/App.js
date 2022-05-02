c43651390cd7fea7cb63f507e52c045ed7ff6dcf
Romiec13 <romiec13@gmail.com>
2022-05-02 10:00:55 -0500

    Added search form component to new file in src
    added Dictionary.js & Dictionary.css to hold search form for dictionary app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
        📚Dictionary App
       </header>
+
+      <main>
+        <Dictionary />
+      </main>
+      
+      <footer className='App-footer'>
+        <small>
+        Coded by <a href='/'>Romie Castilleja</a>
+        </small>
+      </footer>
+      </div>
     </div>
   );
 };
