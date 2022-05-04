f52662f03b0b313bc871eb9d140c2fb30a6b5018
Romiec13 <romiec13@gmail.com>
2022-05-04 10:50:46 -0500

    Updated Meaning.js layout

--- src/Meaning.js
+++ src/Meaning.js
@@ -12,13 +12,13 @@ export default function Meaning(props) {
             <p>
               <strong>Definition:</strong> {definition.definition}
               <br />
-              <strong>Example:</strong>
+              
               <em>{definition.example}</em>
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
-                );
-            })}
-           </div>
+         );
+        })}
+        </div>
     );
 }
