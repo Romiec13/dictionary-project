62c9dacc6ffa3737d31341c1101b9e42c107736b
Romiec13 <romiec13@gmail.com>
2022-05-05 10:50:55 -0500

    Added some CSS styling to Dictionary app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
       </header>
 
       <main>
-        <Dictionary />
+        <Dictionary defaultValue="magic" />
       </main>
       
       <footer className='App-footer'>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,21 +4,26 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState ("");
+export default function Dictionary(props) {
+    let [keyword, setKeyword] = useState (props.defaultValue);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response) {
         console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
-    function search (event) {
-        event.preventDefault();
+    function search () {
 
         //documentation: https://dictionaryapi.dev
         let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
 
@@ -26,12 +31,28 @@ export default function Dictionary() {
         setKeyword(event.target.value);
     }
 
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
     return (
         <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange}/>
+            <section>
+                <h1>What word do you want to look up?</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultValue}/>
             </form>
+            <div className="hint">
+                Suggested words: sunset, wine, forest
+            </div>
+            </section>
             <Results results={results}/>
         </div>
     )
+} else {
+    load();
+    return "loading";
+}
 }
